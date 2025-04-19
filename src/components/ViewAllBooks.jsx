03f5a131eb2e1a517c783b7122340160a90cdfb5
ASWATHY248 <aswathyr451@gmail.com>
2025-04-19 10:58:43 +0530

    last

--- src/components/ViewAllBooks.jsx
+++ src/components/ViewAllBooks.jsx
@@ -14,10 +14,10 @@ const ViewAllBooks = () => {
                             <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
                                 <div class="card">
-                                    <img src="https://m.media-amazon.com/images/I/71bhR8QDlPL.jpg" class="card-img-top" alt="https://m.media-amazon.com/images/I/71bhR8QDlPL.jpg" />
+                                    <img src="https://m.media-amazon.com/images/I/61hJEhXPFvL._SY466_.jpg" class="card-img-top" alt="https://m.media-amazon.com/images/I/61hJEhXPFvL._SY466_.jpg" />
                                     <div class="card-body">
-                                        <h5 class="card-title">Sherlock Holmes</h5>
-                                        <p class="card-text">Some quick example text to build on the Sherlock Holmes and make up the bulk of the card's content.</p>
+                                        <h5 class="card-title">The Girl On The Train</h5>
+                                        <p class="card-text">Some quick example text to build on the The Girl On The Train and make up the bulk of the card's content.</p>
                                         <a href="#" class="btn btn-success">Buy Now</a>
                                     </div>
                                 </div>
@@ -27,10 +27,10 @@ const ViewAllBooks = () => {
 
 
                                 <div class="card">
-                                    <img src="https://m.media-amazon.com/images/I/71bhR8QDlPL.jpg" class="card-img-top" alt="https://m.media-amazon.com/images/I/71bhR8QDlPL.jpg" />
+                                    <img src="https://m.media-amazon.com/images/I/61hJEhXPFvL._SY466_.jpg" class="card-img-top" alt="https://m.media-amazon.com/images/I/61hJEhXPFvL._SY466_.jpg" />
                                     <div class="card-body">
-                                        <h5 class="card-title">Sherlock Holmes</h5>
-                                        <p class="card-text">Some quick example text to build on the Sherlock Holmes and make up the bulk of the card's content.</p>
+                                        <h5 class="card-title">The Girl On The Train</h5>
+                                        <p class="card-text">Some quick example text to build on the The Girl On The Train and make up the bulk of the card's content.</p>
                                         <a href="#" class="btn btn-success">Buy Now</a>
                                     </div>
                                 </div>
@@ -41,10 +41,10 @@ const ViewAllBooks = () => {
                             <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
                                 <div class="card">
-                                    <img src="https://m.media-amazon.com/images/I/71bhR8QDlPL.jpg" class="card-img-top" alt="https://m.media-amazon.com/images/I/71bhR8QDlPL.jpg" />
+                                    <img src="https://m.media-amazon.com/images/I/61hJEhXPFvL._SY466_.jpg" class="card-img-top" alt="https://m.media-amazon.com/images/I/61hJEhXPFvL._SY466_.jpg" />
                                     <div class="card-body">
-                                        <h5 class="card-title">Sherlock Holmes</h5>
-                                        <p class="card-text">Some quick example text to build on the Sherlock Holmes and make up the bulk of the card's content.</p>
+                                        <h5 class="card-title">The Girl On The Train</h5>
+                                        <p class="card-text">Some quick example text to build on the The Girl On The Train and make up the bulk of the card's content.</p>
                                         <a href="#" class="btn btn-success">Buy Now</a>
                                     </div>
                                 </div>
@@ -54,10 +54,10 @@ const ViewAllBooks = () => {
                             <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
                                 <div class="card">
-                                    <img src="https://m.media-amazon.com/images/I/71bhR8QDlPL.jpg" class="card-img-top" alt="https://m.media-amazon.com/images/I/71bhR8QDlPL.jpg" />
+                                    <img src="https://m.media-amazon.com/images/I/61hJEhXPFvL._SY466_.jpg" class="card-img-top" alt="https://m.media-amazon.com/images/I/61hJEhXPFvL._SY466_.jpg" />
                                     <div class="card-body">
-                                        <h5 class="card-title">Sherlock Holmes</h5>
-                                        <p class="card-text">Some quick example text to build on the Sherlock Holmes and make up the bulk of the card's content.</p>
+                                        <h5 class="card-title">The Girl On The Train</h5>
+                                        <p class="card-text">Some quick example text to build on the The Girl On The Train and make up the bulk of the card's content.</p>
                                         <a href="#" class="btn btn-success">Buy Now</a>
                                     </div>
                                 </div>
@@ -67,10 +67,10 @@ const ViewAllBooks = () => {
                             <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
                                 <div class="card">
-                                    <img src="https://m.media-amazon.com/images/I/71bhR8QDlPL.jpg" class="card-img-top" alt="https://m.media-amazon.com/images/I/71bhR8QDlPL.jpg" />
+                                    <img src="https://m.media-amazon.com/images/I/61hJEhXPFvL._SY466_.jpg" class="card-img-top" alt="https://m.media-amazon.com/images/I/61hJEhXPFvL._SY466_.jpg" />
                                     <div class="card-body">
-                                        <h5 class="card-title">Sherlock Holmes</h5>
-                                        <p class="card-text">Some quick example text to build on the Sherlock Holmes and make up the bulk of the card's content.</p>
+                                        <h5 class="card-title">The Girl On The Train</h5>
+                                        <p class="card-text">Some quick example text to build on the The Girl On The Train and make up the bulk of the card's content.</p>
                                         <a href="#" class="btn btn-success">Buy Now</a>
                                     </div>
                                 </div>
@@ -80,10 +80,10 @@ const ViewAllBooks = () => {
                             <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
                                 <div class="card">
-                                    <img src="https://m.media-amazon.com/images/I/71bhR8QDlPL.jpg" class="card-img-top" alt="https://m.media-amazon.com/images/I/71bhR8QDlPL.jpg" />
+                                    <img src="https://m.media-amazon.com/images/I/61hJEhXPFvL._SY466_.jpg" class="card-img-top" alt="https://m.media-amazon.com/images/I/61hJEhXPFvL._SY466_.jpg" />
                                     <div class="card-body">
-                                        <h5 class="card-title">Sherlock Holmes</h5>
-                                        <p class="card-text">Some quick example text to build on the Sherlock Holmes and make up the bulk of the card's content.</p>
+                                        <h5 class="card-title">The Girl On The Train</h5>
+                                        <p class="card-text">Some quick example text to build on the The Girl On The Train and make up the bulk of the card's content.</p>
                                         <a href="#" class="btn btn-success">Buy Now</a>
                                     </div>
                                 </div>
@@ -93,10 +93,10 @@ const ViewAllBooks = () => {
                             <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
                                 <div class="card">
-                                    <img src="https://m.media-amazon.com/images/I/71bhR8QDlPL.jpg" class="card-img-top" alt="https://m.media-amazon.com/images/I/71bhR8QDlPL.jpg" />
+                                    <img src="https://m.media-amazon.com/images/I/61hJEhXPFvL._SY466_.jpg" class="card-img-top" alt="https://m.media-amazon.com/images/I/61hJEhXPFvL._SY466_.jpg" />
                                     <div class="card-body">
-                                        <h5 class="card-title">Sherlock Holmes</h5>
-                                        <p class="card-text">Some quick example text to build on the Sherlock Holmes and make up the bulk of the card's content.</p>
+                                        <h5 class="card-title">The Girl On The Train</h5>
+                                        <p class="card-text">Some quick example text to build on the The Girl On The Train and make up the bulk of the card's content.</p>
                                         <a href="#" class="btn btn-success">Buy Now</a>
                                     </div>
                                 </div>
@@ -106,10 +106,10 @@ const ViewAllBooks = () => {
                             <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
                                 <div class="card">
-                                    <img src="https://m.media-amazon.com/images/I/71bhR8QDlPL.jpg" class="card-img-top" alt="https://m.media-amazon.com/images/I/71bhR8QDlPL.jpg" />
+                                    <img src="https://m.media-amazon.com/images/I/61hJEhXPFvL._SY466_.jpg" class="card-img-top" alt="https://m.media-amazon.com/images/I/61hJEhXPFvL._SY466_.jpg" />
                                     <div class="card-body">
-                                        <h5 class="card-title">Sherlock Holmes</h5>
-                                        <p class="card-text">Some quick example text to build on the Sherlock Holmes and make up the bulk of the card's content.</p>
+                                        <h5 class="card-title">The Girl On The Train</h5>
+                                        <p class="card-text">Some quick example text to build on the The Girl On The Train and make up the bulk of the card's content.</p>
                                         <a href="#" class="btn btn-success">Buy Now</a>
                                     </div>
                                 </div>
@@ -120,10 +120,10 @@ const ViewAllBooks = () => {
 
 
                                 <div class="card">
-                                    <img src="https://m.media-amazon.com/images/I/71bhR8QDlPL.jpg" class="card-img-top" alt="https://m.media-amazon.com/images/I/71bhR8QDlPL.jpg" />
+                                    <img src="https://m.media-amazon.com/images/I/61hJEhXPFvL._SY466_.jpg" class="card-img-top" alt="https://m.media-amazon.com/images/I/61hJEhXPFvL._SY466_.jpg" />
                                     <div class="card-body">
-                                        <h5 class="card-title">Sherlock Holmes</h5>
-                                        <p class="card-text">Some quick example text to build on the Sherlock Holmes and make up the bulk of the card's content.</p>
+                                        <h5 class="card-title">The Girl On The Train</h5>
+                                        <p class="card-text">Some quick example text to build on the The Girl On The Train and make up the bulk of the card's content.</p>
                                         <a href="#" class="btn btn-success">Buy Now</a>
                                     </div>
                                 </div>
@@ -133,10 +133,10 @@ const ViewAllBooks = () => {
                             <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
                                 <div class="card">
-                                    <img src="https://m.media-amazon.com/images/I/71bhR8QDlPL.jpg" class="card-img-top" alt="https://m.media-amazon.com/images/I/71bhR8QDlPL.jpg" />
+                                    <img src="https://m.media-amazon.com/images/I/61hJEhXPFvL._SY466_.jpg" class="card-img-top" alt="https://m.media-amazon.com/images/I/61hJEhXPFvL._SY466_.jpg" />
                                     <div class="card-body">
-                                        <h5 class="card-title">Sherlock Holmes</h5>
-                                        <p class="card-text">Some quick example text to build on the Sherlock Holmes and make up the bulk of the card's content.</p>
+                                        <h5 class="card-title">The Girl On The Train</h5>
+                                        <p class="card-text">Some quick example text to build on the The Girl On The Train and make up the bulk of the card's content.</p>
                                         <a href="#" class="btn btn-success">Buy Now</a>
                                     </div>
                                 </div>
